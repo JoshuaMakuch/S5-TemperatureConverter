b5210f046c1c80dbc02b76805adec895ed128334
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-12 23:10:41 -0600

    Added comments

--- S5-TemperatureConverter/TemperatureConverter.vb
+++ S5-TemperatureConverter/TemperatureConverter.vb
@@ -28,25 +28,28 @@ Public Class TemperatureConverter
         'Prevents a convserion that may result in a crash
         Try
             If TempIsF Then
+                'If the F is pressed, it will assume the input is in C and convert
                 OutputTextBox.Text = CStr(Math.Round(((CInt(InputTextBox.Text) - 32) * 5 / 9), 1))
                 OutputLabel.Text = "Output (F)"
             ElseIf Not TempIsF Then
+                'If the C is pressed, it will assume the input is in F and convert
                 OutputTextBox.Text = CStr(Math.Round(((CInt(InputTextBox.Text) * 5 / 9) + 32), 1))
                 OutputLabel.Text = "Output (C)"
             End If
         Catch ex As Exception
+            'If not a number, output an invalid state
             OutputTextBox.Text = "Invalid Number"
             OutputLabel.Text = "Output (?)"
         End Try
 
     End Sub
 
-    'When the user presses the fahrenheit button
+    'When the user presses the fahrenheit button sets the TempIsF to true
     Private Sub FahrenheitButton_CheckedChanged(sender As Object, e As EventArgs) Handles FahrenheitButton.CheckedChanged
         TempIsF = True
     End Sub
 
-    'When the user presses the celsius button
+    'When the user presses the celsius button sets the TempIsF to false
     Private Sub CelsiusButton_CheckedChanged(sender As Object, e As EventArgs) Handles CelsiusButton.CheckedChanged
         TempIsF = False
     End Sub
